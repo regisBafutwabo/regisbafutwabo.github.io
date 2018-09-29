b9984a490ef51fef0e6ceb44a566b33a98756be3
regisBafutwabo <regiswareja@gmail.com>
2018-09-29 14:05:14 +0900

    added service worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install',function(e) {
+    e.waitUntil(
+        caches.open('regisbafutwabo.github.io').then(function(cache){
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/resume.css',
+                '/css/resume.min.css',
+                '/js/resume.js',
+                '/js/resume.min.js',
+                '/vendor',
+                '/scss'
+            ]);
+})
+);
+});
+
+self.addEventListener('fetch',function(event){
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request);
+        })
+    );
+});
